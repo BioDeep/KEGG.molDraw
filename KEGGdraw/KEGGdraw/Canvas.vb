7bcad6a1217381ae02d43cf4278e599d0a662971
xieguigang <xie.guigang@gmail.com>
2017-05-25 21:15:46 +0800

    simply draw the atoms

--- KEGGdraw/KEGGdraw/Canvas.vb
+++ KEGGdraw/KEGGdraw/Canvas.vb
@@ -1,4 +1,5 @@
-﻿Imports System.Runtime.CompilerServices
+﻿Imports System.Drawing
+Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Imaging.Drawing2D
 Imports Microsoft.VisualBasic.Imaging.Driver
@@ -9,8 +10,19 @@ Public Module Canvas
 
     <Extension>
     Public Function Draw(kcf As KCF, Optional size$ = "800,800", Optional padding$ = g.DefaultPadding, Optional bg$ = "white", Optional font$ = CSSFont.Win7Normal) As GraphicsData
+        Dim atoms = kcf.Atoms _
+            .Select(Function(a)
+                        Return (pt:=a.Atom2D_coordinates.ToPointF(100000), Atom:=a.KEGGAtom)
+                    End Function) _
+            .ToArray
+        Dim dot = Brushes.Gray
         Dim plotInternal =
             Sub(ByRef g As IGraphics, region As GraphicsRegion)
+
+                For Each atom In atoms
+                    Dim pt As New PointF(atom.pt.X, atom.pt.Y * -1)
+                    Call g.FillPie(dot, pt.X * 10, pt.Y * 10, 5, 5, 0, 360)
+                Next
 
             End Sub
 
--- KEGGdraw/KEGGdraw/Module1.vb
+++ KEGGdraw/KEGGdraw/Module1.vb
@@ -5,6 +5,7 @@ Module Module1
     Sub Main()
         Dim kcf As KCF = "G:\KEGG.molDraw\NADPH.txt".LoadKCF
         Call kcf.GetJson.__DEBUG_ECHO
+        Call kcf.Draw().Save("./test.png")
 
         Pause()
     End Sub
