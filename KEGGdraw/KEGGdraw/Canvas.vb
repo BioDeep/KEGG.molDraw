bf16baa63ca0b6d073fa83414ff27f8486425b14
xieguigang <xie.guigang@gcmodeller.org>
2017-07-29 14:13:58 +0800

    parallel shift not success

--- KEGGdraw/KEGGdraw/Canvas.vb
+++ KEGGdraw/KEGGdraw/Canvas.vb
@@ -2,6 +2,7 @@
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Imaging.Drawing2D
+Imports Microsoft.VisualBasic.Imaging.Drawing2D.Vector.Shapes
 Imports Microsoft.VisualBasic.Imaging.Driver
 Imports Microsoft.VisualBasic.MIME.Markup.HTML.CSS
 Imports Microsoft.VisualBasic.Scripting.Runtime
@@ -24,6 +25,7 @@ Public Module Canvas
                          Optional bg$ = "white",
                          Optional font$ = CSSFont.Win7Normal) As GraphicsData
 
+        Dim dot = Brushes.Gray
         Dim atoms As (pt As PointF, atom$)() =
             kcf _
             .Atoms _
@@ -34,7 +36,7 @@ Public Module Canvas
                         End With
                     End Function) _
             .ToArray Or die("No atom elements to plot!", Function(l) DirectCast(l, Array).Length = 0)
-        Dim dot = Brushes.Gray
+
         Dim plotInternal =
             Sub(ByRef g As IGraphics, region As GraphicsRegion)
                 Dim bounds = atoms.Select(Function(a) a.pt).GetBounds
@@ -59,17 +61,15 @@ Public Module Canvas
                     Call g.FillPie(dot, pt.X, pt.Y, 5, 5, 0, 360)
                 Next
 
-                Dim bold As New Pen(Color.Black, 3)
-
-                For Each bound In kcf.Bounds
+                For Each bound As Bound In kcf.Bounds
                     Dim a = atoms(bound.from - 1).pt.OffSet2D(centra)
                     Dim b = atoms(bound.to - 1).pt.OffSet2D(centra)
+                    Dim line As New Line(a, b)
 
-                    If bound.bounds > 1 Then
-                        Call g.DrawLine(bold, a, b)
-                    Else
-                        Call g.DrawLine(Pens.Black, a, b)
-                    End If
+                    For i As Integer = 1 To bound.bounds
+                        Call line.Draw(g)
+                        line = line.ParallelShift(10)
+                    Next
                 Next
             End Sub
 
