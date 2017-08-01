7e000acf13e42c36d2ddc4dfbfc3b28f0a50f9f2
xieguigang <xie.guigang@gcmodeller.org>
2017-08-01 12:22:05 +0800

    kegg atom types

---
+++ KEGGdraw/KEGGdraw/KegAtomType.vb
@@ -0,0 +1,42 @@
+﻿Imports Microsoft.VisualBasic.Text
+
+Public Structure KegAtomType
+
+    Dim code$
+    Dim formula$
+    Dim name$
+    Dim type As Types
+
+    Sub New(code$, formula$, name$, type As Types)
+        Me.code = code
+        Me.formula = formula
+        Me.name = name
+        Me.type = type
+    End Sub
+
+    Public Overrides Function ToString() As String
+        Return $"{code}    {formula} / {name}"
+    End Function
+
+    Public Enum Types As Byte
+        Other
+        Carbon = 12
+        Nitrogen = 14
+        Oxygen = 16
+        Sulfur = 32
+        Phosphorus = 31
+        ''' <summary>
+        ''' 卤族元素
+        ''' </summary>
+        Halogen = 255
+    End Enum
+
+    Public Shared ReadOnly Property KEGGAtomTypes As New Dictionary(Of String, KegAtomType)
+
+    Shared Sub New()
+        With KEGGAtomTypes
+
+        End With
+
+    End Sub
+End Structure
