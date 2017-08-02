ad9a81e2e3a32624789d5e285b267af551a18b9b
xieguigang <xie.guigang@gcmodeller.org>
2017-08-02 23:21:35 +0800

    add stroke style

--- KEGGdraw/Module1.vb
+++ KEGGdraw/Module1.vb
@@ -4,9 +4,9 @@ Module Module1
 
     Sub Main()
 
-        Dim kcf As KCF = "../../../../NADPH.txt".LoadKCF
+        Dim kcf As KCF = "../DATA/NADPH.txt".LoadKCF
         Call kcf.GetJson.__DEBUG_ECHO
-        Call kcf.Draw().Save("../../../../NADPH.png")
+        Call kcf.Draw().Save("../DATA/NADPH.png")
 
         Pause()
     End Sub
