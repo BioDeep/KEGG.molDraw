b6a84941455817adcb4b56c994eba8995acf4548
xieguigang <xie.guigang@gcmodeller.org>
2017-08-03 12:19:32 +0800

    add CLI module

---
+++ KEGGdraw/CLI.vb
@@ -0,0 +1,33 @@
+﻿Imports System.ComponentModel
+Imports Microsoft.VisualBasic.CommandLine
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+
+Module CLI
+
+    <ExportAPI("/draw.kcf")>
+    <Usage("/draw.kcf /in <kcf.txt> [/out <out.png>]")>
+    Public Function DrawKCF(args As CommandLine) As Integer
+        Dim in$ = args <= "/in"
+        Dim out$ = args.GetValue("/out", [in].TrimSuffix & ".png")
+        Dim kcf As KCF = [in].LoadKCF
+
+        Return kcf.Draw() _
+            .Save(out) _
+            .CLICode
+    End Function
+
+    <ExportAPI("/draw.kegg")>
+    <Description("Drawing query data from KEGG dbget API.")>
+    <Usage("/draw.kegg /cpd <kegg_compound_ID> [/out <out.DIR>]")>
+    Public Function DrawKEGG(args As CommandLine) As Integer
+        Dim cpd$ = args <= "/cpd"
+        Dim out$ = args.GetValue("/out", App.CurrentDirectory)
+    End Function
+
+    <ExportAPI("/dump.kegg.compounds")>
+    <Description("Dumping the KEGG compounds database")>
+    <Usage("/dump.kegg.compounds [/out <save_dir>]")>
+    Public Function DumpKEGGCompounds(args As CommandLine) As Integer
+        Dim out$ = args.GetValue("/out", App.CurrentDirectory & "/KEGG.compounds/")
+    End Function
+End Module
--- KEGGdraw/Program.vb
+++ KEGGdraw/Program.vb
@@ -1,12 +1,24 @@
 ﻿Imports Microsoft.VisualBasic.Serialization.JSON
 
-Module Module1
+Module Program
 
-    Sub Main()
+    Public Function Main() As Integer
+        Return GetType(CLI).RunCLI(App.CommandLine)
+    End Function
+
+    Sub New()
+#If DEBUG Then
+        Call test()
+#End If
+    End Sub
+
+    Sub test()
 
         Dim kcf As KCF = "../../../../NADPH.txt".LoadKCF
         Call kcf.GetJson.__DEBUG_ECHO
         Call kcf.Draw().Save("../../../../NADPH.png")
+
+
 
         Pause()
     End Sub
