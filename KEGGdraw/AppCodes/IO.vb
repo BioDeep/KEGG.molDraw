4e372625b97159bf8a72d954675419aea0356519
xieguigang <xie.guigang@gcmodeller.org>
2017-08-09 10:52:18 +0800

    fix for ignores

---
+++ KEGGdraw/AppCodes/IO.vb
@@ -0,0 +1,81 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Text.Xml.Models
+
+''' <summary>
+''' KCF molecular structure file IO provider
+''' </summary>
+Public Module IO
+
+    ''' <summary>
+    ''' Load a text file and creates a KCF model from this data
+    ''' </summary>
+    ''' <param name="stream$">Can be text data or file path</param>
+    ''' <returns></returns>
+    <Extension> Public Function LoadKCF(stream$) As KCF
+        If stream.FileExists Then
+            stream = stream.ReadAllText Or die("No content data!", Function(s) DirectCast(s, String).StringEmpty)
+        End If
+
+        Dim lines$() = stream.lTokens
+        Dim sections = lines _
+            .Split(Function(s) Not InStr(s, "    ") = 1, DelimiterLocation.NextFirst) _
+            .Skip(1) _
+            .ToArray
+        Dim t$() = sections(Scan0) _
+            .First _
+            .StringSplit("\s+") _
+            .Where(Function(s) Not s.StringEmpty) _
+            .ToArray
+        Dim entry As New Entry With {
+            .Id = t(1),
+            .Type = t(2)
+        }
+        Dim atoms = sections(1) _
+            .Skip(1) _
+            .ToArray _
+            .parseAtoms
+        Dim bounds = sections(2) _
+            .Skip(1) _
+            .ToArray _
+            .parseBounds
+
+        Return New KCF With {
+            .Entry = entry,
+            .Atoms = atoms,
+            .Bounds = bounds
+        }
+    End Function
+
+    <Extension> Private Function parseAtoms(atoms$()) As Atom()
+        Return atoms _
+            .Select(AddressOf Trim) _
+            .Select(Function(s) s.StringSplit("\s+")) _
+            .Select(Function(t)
+                        Return New Atom With {
+                            .Index = Val(t(0)),
+                            .KEGGAtom = t(1),
+                            .Atom = t(2),
+                            .Atom2D_coordinates = New Coordinate With {
+                                .X = Val(t(3)),
+                                .Y = Val(t(4))
+                            }
+                        }
+                    End Function) _
+            .ToArray
+    End Function
+
+    <Extension> Private Function parseBounds(bounds$()) As Bound()
+        Return bounds _
+            .Select(AddressOf Trim) _
+            .Select(Function(s) s.StringSplit("\s+")) _
+            .Select(Function(t)
+                        Return New Bound With {
+                            .from = Val(t(1)),
+                            .to = Val(t(2)),
+                            .bounds = Val(t(3)),
+                            .dimentional_levels = t.ElementAtOrDefault(4)
+                        }
+                    End Function) _
+            .ToArray
+    End Function
+End Module
---
+++ KEGGdraw/AppCodes/KCF.vb
@@ -0,0 +1,71 @@
+﻿Imports System.Xml
+Imports System.Xml.Schema
+Imports System.Xml.Serialization
+Imports Microsoft.VisualBasic.Text.Xml.Models
+
+Public Class KCF
+
+    Public Property Entry As Entry
+    Public Property Atoms As Atom()
+    Public Property Bounds As Bound()
+
+End Class
+
+Public Structure Entry : Implements IXmlSerializable
+
+    Public Property Id As String
+    Public Property Type As String
+
+    Public Sub ReadXml(reader As XmlReader) Implements IXmlSerializable.ReadXml
+        Id = reader.GetAttribute("KEGG.id")
+        Type = reader.GetAttribute("KEGG.type")
+    End Sub
+
+    Public Sub WriteXml(writer As XmlWriter) Implements IXmlSerializable.WriteXml
+        Call writer.WriteAttributeString("KEGG.id", Id)
+        Call writer.WriteAttributeString("KEGG.type", Type)
+    End Sub
+
+    Public Overrides Function ToString() As String
+        Return $"Dim {Id} As {Type}"
+    End Function
+
+    Public Function GetSchema() As XmlSchema Implements IXmlSerializable.GetSchema
+        Return Nothing
+    End Function
+End Structure
+
+Public Structure Atom
+
+    Public Property Index As Integer
+    Public Property KEGGAtom As String
+    Public Property Atom As String
+    ''' <summary>
+    ''' 由于这个坐标对象只能存储<see cref="Integer"/>类型，
+    ''' 所以KCF之中的坐标值需要乘以100000之后再保存在这个属性值中
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Atom2D_coordinates As Coordinate
+
+    Public Overrides Function ToString() As String
+        Return $"[{Index}] {KEGGAtom} --> {Atom2D_coordinates.ToString}"
+    End Function
+
+End Structure
+
+Public Structure Bound
+
+    Public Property from As Integer
+    Public Property [to] As Integer
+    Public Property bounds As Integer
+    ''' <summary>
+    ''' 化学键两边的官能团的空间层次
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property dimentional_levels As String
+
+    Public Overrides Function ToString() As String
+        Return $"{from} => {[to]}, {bounds} bounds; {dimentional_levels}"
+    End Function
+
+End Structure
