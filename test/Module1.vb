ed50b1c451d0de4d3c47bf686d7d50c27ef9ad19
xieguigang <xie.guigang@gcmodeller.org>
2017-12-23 22:13:56 +0800

    add drawing layer test

--- test/Module1.vb
+++ test/Module1.vb
@@ -1,4 +1,7 @@
-﻿Imports KEGGdraw
+﻿Imports System.Drawing
+Imports KEGGdraw
+Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Imaging.BitmapImage
 Imports Microsoft.VisualBasic.Serialization.JSON
 Imports SMRUCC.Chemistry.Model
 Imports SMRUCC.Chemistry.Model.Graph
@@ -7,6 +10,7 @@ Module Module1
 
     Sub Main()
 
+        Call layerSourceTest()
         Call drawTest()
 
         Dim list = KegAtomType.KEGGAtomTypes.GetJson
@@ -30,6 +34,19 @@ Module Module1
 
         KCF = IO.LoadKCF("../DATA/CoA.txt")
         Call KCF.Draw().Save("../DATA/CoA.png")
+
+        Pause()
+    End Sub
+
+    Sub layerSourceTest()
+        Dim KCF = IO.LoadKCF("../DATA/Tetrahydrofolate.txt")
+        Dim image = KCF.Draw().AsGDIImage
+
+        Dim tr = image.ColorReplace(Color.White, Color.Transparent)
+
+        Call tr.SaveAs("./ttttt.png")
+
+        Call tr.ResizeUnscaledByHeight(500).SaveAs("./fdffsfsd.png")
 
         Pause()
     End Sub
