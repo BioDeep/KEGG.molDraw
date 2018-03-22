3326292f4641633eb1e0d8bde80e344a2356e9f7
xieguigang <xie.guigang@gcmodeller.org>
2018-03-22 10:38:42 +0800

    cluster of the KCF modles

---
+++ KCF.Graph/Cluster.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Math.LinearAlgebra
+
+''' <summary>
+''' Cluster of the chemical compounds based on the binary tree graph
+''' </summary>
+Public Module Cluster
+
+    <Extension> Public Function KCFAtomVector(KCF As Model.KCF) As Vector
+
+    End Function
+End Module
