b00ead9d7816b17a8c33ef413bde519b89e33c06
xieguigang <xie.guigang@gcmodeller.org>
2018-12-05 13:08:10 +0800

    improvements of the drawer cli

--- KEGGdraw/CLI.vb
+++ KEGGdraw/CLI.vb
@@ -65,7 +65,7 @@ Imports SMRUCC.Chemistry.Model
         Dim corpBlank As Boolean = args("/corp")
 
         If [in].DirectoryExists Then
-            Dim EXPORT$ = args("/out") Or "./"
+            Dim EXPORT$ = args("/out") Or $"{[in].TrimDIR}.images/"
 
             For Each file As String In ls - l - r - {"*.txt", "*.kcf"} <= [in]
                 Dim out$ = EXPORT & "/" & file.BaseName & ".png"
@@ -97,50 +97,5 @@ Imports SMRUCC.Chemistry.Model
             .SaveAs(out, ImageFormats.Png) _
             .CLICode
     End Function
-
-    '<ExportAPI("/draw.kegg")>
-    '<Description("Drawing query data from KEGG dbget API.")>
-    '<Usage("/draw.kegg /cpd <kegg_compound_ID> [/out <out.DIR>]")>
-    '<Group(Groups.KCF_tools)>
-    'Public Function DrawKEGG(args As CommandLine) As Integer
-    '    Dim cpd$ = args <= "/cpd"
-    '    Dim out$ = args.GetValue("/out", App.CurrentDirectory)
-    '    Dim KCF = Compound.DownloadKCF(cpd, out).LoadKCF
-
-    '    Return KCF.Draw() _
-    '        .Save(out) _
-    '        .CLICode
-    'End Function
-
-    '<ExportAPI("/dump.kegg.compounds")>
-    '<Description("Dumping the KEGG compounds database")>
-    '<Usage("/dump.kegg.compounds [/max.cid <default=25000> /out <save_dir>]")>
-    '<Group(Groups.KegDatabase)>
-    'Public Function DumpKEGGCompounds(args As CommandLine) As Integer
-    '    With args.GetValue("/out", App.CurrentDirectory & "/KEGG.compounds/")
-    '        Call cpdBriet.WorkspaceCleanup(DIR:= .ref)
-    '        Return cpdBriet.DownloadFromResource(EXPORT:= .ref,
-    '                                             structInfo:=True,
-    '                                             maxID:=args.GetValue("/max.cid", 25000)) _
-    '            .GetJson _
-    '            .SaveTo(.ref & "/failures.json") _
-    '            .CLICode
-    '    End With
-    'End Function
-
-    '<ExportAPI("/dump.kegg.maps")>
-    '<Description("Dumping the KEGG maps database for human species.")>
-    '<Usage("/dump.kegg.maps [/out <save_dir>]")>
-    '<Group(Groups.KegDatabase)>
-    'Public Function DumpKEGGMaps(args As CommandLine) As Integer
-    '    With args.GetValue("/out", App.CurrentDirectory & "/KEGG.pathwayMaps/")
-    '        Dim tmp = App.GetAppSysTempFile(".txt")
-    '        Call My.Resources.hsa00001.FlushStream(tmp)
-    '        Return kegMap.Downloads(EXPORT:= .ref, briefFile:=tmp) _
-    '            .GetJson _
-    '            .SaveTo(.ref & "/failures.json") _
-    '            .CLICode
-    '    End With
-    'End Function
 End Module
 
