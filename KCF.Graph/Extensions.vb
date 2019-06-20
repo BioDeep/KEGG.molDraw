3236c698fea2804c238aff71ca293af8161f9775
xieguigang <xie.guigang@gcmodeller.org>
2019-06-20 18:49:01 +0800

    Update Extensions.vb
    add dev note about calculate charge value of the atom group

--- KCF.Graph/Extensions.vb
+++ KCF.Graph/Extensions.vb
@@ -5,6 +5,10 @@ Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
 <HideModuleName>
 Public Module Extensions
 
+    ' 可以通过KEGG的原子基团解析出有多少个化学键
+    ' 然后通过分析与当前的这个节点相连接的边的化学键的数量
+    ' 二者的差值即为当前的这个原子基团可能的电荷值
+
     ''' <summary>
     ''' Calculate the charge value of ion graph model
     ''' </summary>
