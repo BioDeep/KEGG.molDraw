2fb8f4849f8b29a33c1f393ebb6c99031e1a413f
xieguigang <xie.guigang@gcmodeller.org>
2019-06-21 09:32:13 +0800

    Update KCF.vb
    add initial charge value

--- KCF.Graph/KCF.vb
+++ KCF.Graph/KCF.vb
@@ -1,51 +1,53 @@
 ﻿#Region "Microsoft.VisualBasic::cccfe22e4aea3215afdd476872a49876, KCF\KCF.Graph\KCF.vb"
 
-    ' Author:
-    ' 
-    '       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
-    ' 
-    ' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
-    ' 
-    ' 
-    ' MIT License
-    ' 
-    ' 
-    ' Permission is hereby granted, free of charge, to any person obtaining a copy
-    ' of this software and associated documentation files (the "Software"), to deal
-    ' in the Software without restriction, including without limitation the rights
-    ' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    ' copies of the Software, and to permit persons to whom the Software is
-    ' furnished to do so, subject to the following conditions:
-    ' 
-    ' The above copyright notice and this permission notice shall be included in all
-    ' copies or substantial portions of the Software.
-    ' 
-    ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    ' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    ' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    ' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    ' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    ' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    ' SOFTWARE.
+' Author:
+' 
+'       xieguigang (gg.xie@bionovogene.com, BioNovoGene Co., LTD.)
+' 
+' Copyright (c) 2018 gg.xie@bionovogene.com, BioNovoGene Co., LTD.
+' 
+' 
+' MIT License
+' 
+' 
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+' 
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
 
 
 
-    ' /********************************************************************************/
+' /********************************************************************************/
 
-    ' Summaries:
+' Summaries:
 
-    ' Module KCF
-    ' 
-    '     Function: Graph
-    ' 
-    ' /********************************************************************************/
+' Module KCF
+' 
+'     Function: Graph
+' 
+' /********************************************************************************/
 
 #End Region
 
+Imports System.Drawing
 Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
 Imports Microsoft.VisualBasic.Data.visualize.Network.Layouts
+Imports Microsoft.VisualBasic.Imaging
 Imports Microsoft.VisualBasic.Language
 
 ''' <summary>
@@ -79,7 +81,10 @@ Imports Microsoft.VisualBasic.Language
                 .Label = $"#{atom.Index}",
                 .data = New NodeData With {
                     .label = atom.KEGGAtom.code,
-                    .initialPostion = point
+                    .initialPostion = point,
+                    .Properties = New Dictionary(Of String, String) From {
+                        {"charge", 0}
+                    }
                 }
             }
 
@@ -93,18 +98,19 @@ Imports Microsoft.VisualBasic.Language
         Dim length#
         Dim node1, node2 As Node
         Dim label$
+        Dim x, y As Point
+        Dim hasLevel As Boolean
 
         For Each bound As Bound In KCF.Bounds
             a = "#" & bound.from
             b = "#" & bound.to
             node1 = nodes(a)
             node2 = nodes(b)
-            length = Imaging.Math2D.Distance(
-                node1.data.initialPostion.Point2D,
-                node2.data.initialPostion.Point2D)
-
-            label = $"{a} -> {b}" Or
-                    $"{a} -> {b} ({bound.dimentional_levels})".When(Not bound.dimentional_levels.StringEmpty)
+            x = node1.data.initialPostion.Point2D
+            y = node2.data.initialPostion.Point2D
+            length = Math2D.Distance(x, y)
+            hasLevel = Not bound.dimentional_levels.StringEmpty
+            label = $"{a} -> {b}" Or $"{a} -> {b} ({bound.dimentional_levels})".When(hasLevel)
 
             edge = New Edge With {
                 .U = node1,
