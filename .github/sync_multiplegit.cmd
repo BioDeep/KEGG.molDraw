21f96f00cf2893b614af17e8dddbf80e690df3e1
xieguigang <xie.guigang@gcmodeller.org>
2022-11-20 01:08:16 +0800

    Update sync_multiplegit.cmd

--- .github/sync_multiplegit.cmd
+++ .github/sync_multiplegit.cmd
@@ -1,9 +1,10 @@
 @echo off
 
+git pull gitee HEAD
 git pull local HEAD
-REM git pull gitee HEAD
 
+git push gitee HEAD
 git push local HEAD
-REM git push gitee HEAD
+
 
 echo synchronization of this code repository job done!
